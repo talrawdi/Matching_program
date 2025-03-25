59ec22f1f8aa8ba5790b236810c915b1954a4ebb
talrawdi <omersara30@gmail.com>
2025-03-25 07:56:32 +0300

    Updat db file

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "products_db2";
+$servername = "sql11b.byadjuster.com";  // تغيير اسم الخادم
+$username = "38205864";                // اسم المستخدم الجديد
+$password = "Mtn733122268";    // كلمة المرور الجديدة
+$dbname = "products_db2";                  // اسم قاعدة البيانات الجديد
 
 // إنشاء الاتصال
 $conn = new mysqli($servername, $username, $password, $dbname);
